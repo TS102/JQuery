6aa11021bb0e47786ef85288bc563ccafcadcc6d
Tyler Sun <tylersun10@gmail.com>
2022-05-15 21:59:03 -0600

    JQ

---
+++ app.js
@@ -0,0 +1,8 @@
+$('.navItem').css(color, 'white');
+console.log('navItem')
+$('#nav').css(background-color, '#145');
+$('li:even').css(background-color, '#ccc');
+$('li:odd').css(background-color, '#aaa');
+let name = $('input[type = "placeholder"]')
+$('name').text(Tyler)
+console.log('name')
